c74295db77e08c5220ceb93270603b7a5198e6e4
Thiago Verney <thiago.verney@gmail.com>
2017-02-07 10:42:00 -0200

    commit inicial

---
+++ index.js
@@ -0,0 +1,5 @@
+'use strict';
+
+hexo.extend.filter.register('before_generate', function(app){
+	  console.log(app);
+});
---
+++ test/index.js
@@ -0,0 +1,11 @@
+var should = require('chai').should(),
+    hexoContentApi = require('../index'),
+    init = hexoContentApi.init;
+
+describe('#escape', function() {
+  it('converts & into &amp;', function() {
+    init();
+  });
+
+});
+
