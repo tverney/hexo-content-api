26d3f0788aa4e5560e6c5dc6cc139be82ddf7942
Thiago Verney <thiago.verney@gmail.com>
2017-02-08 11:42:28 -0200

    adicionado como dependência de produção

--- test/index.js
+++ test/index.js
@@ -6,36 +6,36 @@ var should = require('chai').should(),
     sync = hexoIndex.sync;
 
 describe('#sync', function() {
-  // it('Init sync with Netzei API data - index.js', function() {
-  // 	var netzei = {
-  // 		url:"https://api.netzei.com",
-  // 		access_token:"yLY0teWHhnretce98JgKCeLp2UogG6DJ"
-  // 	}
-  // 	hexo.config.netzei = netzei;
-  //   sync(hexo);
-  // });
+  it('Init sync with Netzei API data - index.js', function() {
+  	var netzei = {
+  		url:"https://api.netzei.com",
+  		access_token:"yLY0teWHhnretce98JgKCeLp2UogG6DJ"
+  	}
+  	hexo.config.netzei = netzei;
+    sync(hexo);
+  });
 
-  // it('getSites - api.js', function() {
-  // 	var netzei = {
-  // 		url:"http://0.0.0.0:9000",
-  // 		access_token:"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjU4OWEwZjgwOTQ4OWJiNTBhNDU1ODhhNiIsImlhdCI6MTQ4NjQ5Mzg0MH0.GwhNyT1oVw61rZMAwnuTPC88rIhyLYp742kVpr5AeLc"
-  // 	}
-  // 	hexo.config.netzei = netzei;
-  //   return api().getContent(hexo.config.netzei, "sites").then(function (data) {
+  it('getSites - api.js', function() {
+  	var netzei = {
+  		url:"http://0.0.0.0:9000",
+  		access_token:"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjU4OWEwZjgwOTQ4OWJiNTBhNDU1ODhhNiIsImlhdCI6MTQ4NjQ5Mzg0MH0.GwhNyT1oVw61rZMAwnuTPC88rIhyLYp742kVpr5AeLc"
+  	}
+  	hexo.config.netzei = netzei;
+    return api().getContent(hexo.config.netzei, "sites").then(function (data) {
     	
-  //   });
-  // });
+    });
+  });
 
-  // it('getPosts - api.js', function() {
-  //   var netzei = {
-  //     url:"http://0.0.0.0:9000",
-  //     access_token:"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjU4OWEwZjgwOTQ4OWJiNTBhNDU1ODhhNiIsImlhdCI6MTQ4NjQ5Mzg0MH0.GwhNyT1oVw61rZMAwnuTPC88rIhyLYp742kVpr5AeLc"
-  //   }
-  //   hexo.config.netzei = netzei;
-  //   return api().getContent(hexo.config.netzei, "posts").then(function (data) {
+  it('getPosts - api.js', function() {
+    var netzei = {
+      url:"http://0.0.0.0:9000",
+      access_token:"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjU4OWEwZjgwOTQ4OWJiNTBhNDU1ODhhNiIsImlhdCI6MTQ4NjQ5Mzg0MH0.GwhNyT1oVw61rZMAwnuTPC88rIhyLYp742kVpr5AeLc"
+    }
+    hexo.config.netzei = netzei;
+    return api().getContent(hexo.config.netzei, "posts").then(function (data) {
      
-  //   });
-  // });
+    });
+  });
 
   it('sync - api.js', function() {
     var netzei = {
