de2f178b4e226fa9f9569ef3b10f2957c2467d61
Thiago Verney <thiago.verney@gmail.com>
2017-06-19 00:37:33 -0300

    adicionada imagem

--- constants.js
+++ constants.js
@@ -3,6 +3,7 @@
 exports.title = 'simple-text';
 exports.content = ['multi-text', 'rich-text'];
 exports.file = 'file-attachment';
+exports.image = 'image';
 exports.gallery = 'image-gallery';
 exports.video = 'video';
 exports.date = 'data';
